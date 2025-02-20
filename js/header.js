a86be6fcfb34e1d1a3cb3277a04de7030a80e802
caninecrew <78000824+caninecrew@users.noreply.github.com>
2025-02-20 16:05:59 -0600

    header
    visible header and centered

--- js/header.js
+++ js/header.js
@@ -4,33 +4,35 @@ document.addEventListener("DOMContentLoaded", function() {
                        window.location.pathname.endsWith('index.html');
 
     const headerContent = isIndexPage ? `
-        <link rel="stylesheet" href="css/styles.css">
-        <nav class="navbar">
-            <div class="logo">Samuel Rumbley</div>
-            <ul class="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="pages/education.html">My Education</a></li>
-                <li><a href="pages/experience.html">Professional Experience</a></li>
-                <li><a href="pages/scouting.html">Scouting Achievements</a></li>
-                <li><a href="pages/church.html">Community Service</a></li>
-                <li><a href="pages/projects.html">My Projects</a></li>
-                <li><a href="pages/achievements.html">Awards & Honors</a></li>
-            </ul>
-        </nav>
+        <header class="site-header">
+            <nav class="navbar">
+                <div class="logo">Samuel Rumbley</div>
+                <ul class="nav-links">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="pages/education.html">My Education</a></li>
+                    <li><a href="pages/experience.html">Professional Experience</a></li>
+                    <li><a href="pages/scouting.html">Scouting Achievements</a></li>
+                    <li><a href="pages/church.html">Community Service</a></li>
+                    <li><a href="pages/projects.html">My Projects</a></li>
+                    <li><a href="pages/achievements.html">Awards & Honors</a></li>
+                </ul>
+            </nav>
+        </header>
     ` : `
-        <link rel="stylesheet" href="../css/styles.css">
-        <nav class="navbar">
-            <div class="logo">Samuel Rumbley</div>
-            <ul class="nav-links">
-                <li><a href="../index.html">Home</a></li>
-                <li><a href="education.html">My Education</a></li>
-                <li><a href="experience.html">Professional Experience</a></li>
-                <li><a href="scouting.html">Scouting Achievements</a></li>
-                <li><a href="church.html">Community Service</a></li>
-                <li><a href="projects.html">My Projects</a></li>
-                <li><a href="achievements.html">Awards & Honors</a></li>
-            </ul>
-        </nav>
+        <header class="site-header">
+            <nav class="navbar">
+                <div class="logo">Samuel Rumbley</div>
+                <ul class="nav-links">
+                    <li><a href="../index.html">Home</a></li>
+                    <li><a href="education.html">My Education</a></li>
+                    <li><a href="experience.html">Professional Experience</a></li>
+                    <li><a href="scouting.html">Scouting Achievements</a></li>
+                    <li><a href="church.html">Community Service</a></li>
+                    <li><a href="projects.html">My Projects</a></li>
+                    <li><a href="achievements.html">Awards & Honors</a></li>
+                </ul>
+            </nav>
+        </header>
     `;
 
     document.querySelector("#header").innerHTML = headerContent;
